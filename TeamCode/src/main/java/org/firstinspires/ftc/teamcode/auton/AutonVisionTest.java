55233f34b0689cd05447197cbd54291ada17ea1e
Trobots <wlhsroboticsteam@gmail.com>
2023-10-11 18:55:21 -0400

    jas  vision test auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonVisionTest.java
@@ -0,0 +1,194 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.common.Settings;
+import org.firstinspires.ftc.teamcode.hardware.DriveTrain;
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+import java.util.List;
+
+
+//@Disabled
+@Autonomous(name = "Test_Vision", group = "Auton")
+// @Autonomous(...) is the other common choice
+
+public class AutonVisionTest extends OpMode {
+
+    //RobotComp robot = new RobotComp();
+    Robot robot = new Robot();
+    private stage currentStage = stage._unknown;
+    // declare auton power variables
+    //private double AUTO_DRIVE_TURBO_SPEED = DriveTrain.DRIVETRAIN_TURBOSPEED;
+    //private double AUTO_DRIVE_SLOW_SPEED = DriveTrain.DRIVETRAIN_SLOWSPEED;
+   // private double AUTO_DRIVE_NORMAL_SPEED = DriveTrain.DRIVETRAIN_NORMALSPEED;
+   // private double AUTO_TURN_SPEED = DriveTrain.DRIVETRAIN_TURNSPEED;
+
+    final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
+//  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+    //  applied to the drive motors to correct the error.
+    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+    final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+    boolean targetFound     = false;    // Set to true when an AprilTag target is detected
+    double  drive           = 0;        // Desired forward power/speed (-1 to +1)
+    double  strafe          = 0;        // Desired strafe power/speed (-1 to +1)
+    double  turn            = 0;        // Desired turning power/speed (-1 to +1)
+    private String RTAG = "8492-Auton";
+    private static final int DESIRED_TAG_ID = 0;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    private static final boolean USE_WEBCAM = true;  // Set true to use a webcam, or false for a phone camera
+    private VisionPortal visionPortal;               // Used to manage the video source.
+    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
+    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        //----------------------------------------------------------------------------------------------
+        // These constants manage the duration we allow for callbacks to user code to run for before
+        // such code is considered to be stuck (in an infinite loop, or wherever) and consequently
+        // the robot controller application is restarted. They SHOULD NOT be modified except as absolutely
+        // necessary as poorly chosen values might inadvertently compromise safety.
+        //----------------------------------------------------------------------------------------------
+        msStuckDetectInit = Settings.msStuckDetectInit;
+        msStuckDetectInitLoop = Settings.msStuckDetectInitLoop;
+        msStuckDetectStart = Settings.msStuckDetectStart;
+        msStuckDetectLoop = Settings.msStuckDetectLoop;
+        msStuckDetectStop = Settings.msStuckDetectStop;
+
+        robot.hardwareMap = hardwareMap;
+        robot.telemetry = telemetry;
+        robot.init();
+        telemetry.addData("Test Vision", "Initialized");
+        initAprilTag();
+        //Initialize Gyro
+        robot.driveTrain.ResetGyro();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+        // initialize robot
+        robot.init_loop();
+    }
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        // start robot
+        runtime.reset();
+        robot.start();
+    }
+
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initAprilTag() {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Create the vision portal by using a builder.
+        if (USE_WEBCAM) {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                    .addProcessor(aprilTag)
+                    .build();
+        } else {
+            visionPortal = new VisionPortal.Builder()
+                    .setCamera(BuiltinCameraDirection.BACK)
+                    .addProcessor(aprilTag)
+                    .build();
+        }
+    }
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        telemetry.addData("Auton_Current_Stage ", currentStage);
+        robot.loop();
+
+        switch (currentStage){
+            case  _unknown:
+                currentStage = stage._00_preStart;
+                break;
+            case _00_preStart:
+                currentStage = stage._10_Drive_To_Tag;
+                break;
+            case _10_Drive_To_Tag:
+                targetFound = false;
+                // Step through the list of detected tags and look for a matching tag
+                List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+                for (AprilTagDetection detection : currentDetections) {
+                    if ((detection.metadata != null) &&
+                            ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID))  ){
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;  // for loop don't look any further.
+                    } else {
+                        telemetry.addData("Unknown Target", "Tag ID %d is not in TagLibrary\n", detection.id);
+                    }
+                }
+                if (! targetFound) {
+                    currentStage = stage._100_End;
+                    robot.driveTrain.stop();
+                }
+                else {
+                    // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
+                    double rangeError = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+                    double headingError = desiredTag.ftcPose.bearing;
+                    double yawError = desiredTag.ftcPose.yaw;
+                    // Use the speed and turn "gains" to calculate how we want the robot to move.
+                    drive = Range.clip(rangeError * SPEED_GAIN, robot.driveTrain.minPower, robot.driveTrain.maxPower);
+                    turn = Range.clip(headingError * TURN_GAIN, robot.driveTrain.minPower, robot.driveTrain.maxPower);
+                    strafe = Range.clip(-yawError * STRAFE_GAIN, robot.driveTrain.minPower, robot.driveTrain.maxPower);
+                    // need a real drive, strafe turn cmd
+                    robot.driveTrain.cmdTeleOp(drive, strafe, turn, .5);
+                }
+                break;
+
+            case _100_End:
+                if(robot.driveTrain.getCmdComplete()){
+                    robot.stop();
+                }
+                break;
+        }
+
+    }  //  loop
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        robot.stop();
+    }
+
+    private enum stage {
+        _unknown,
+        _00_preStart,
+        _10_Drive_To_Tag,
+        _100_End
+
+
+    }
+}
